1e31456b80189e64516325d43098ff35b99a45c5
fpellet <florent.pellet@gmail.com>
2015-04-08 22:36:29 +0200

    initialize web project

---
+++ Bbl.KnockoutJs/Bbl.KnockoutJs/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+using Owin;
+
+namespace Bbl.KnockoutJs
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters => View["index"];
+        }
+    }
+}
