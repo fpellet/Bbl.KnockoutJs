0361cc70a18cb4df41c92e0bfcc1d8e3ee390de3
fpellet <florent.pellet@gmail.com>
2015-04-08 23:23:23 +0200

    Add url to get unicorns

--- Bbl.KnockoutJs/Bbl.KnockoutJs/Startup.cs
+++ Bbl.KnockoutJs/Bbl.KnockoutJs/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Nancy;
+﻿using System.IO;
+using System.Linq;
+using System.Web.Hosting;
+using Nancy;
 using Owin;
 
 namespace Bbl.KnockoutJs
@@ -13,9 +16,33 @@ namespace Bbl.KnockoutJs
 
     public class IndexModule : NancyModule
     {
+        private const string UnicornsPath = "/Content/Unicorn/";
+
         public IndexModule()
         {
             Get["/"] = parameters => View["index"];
+
+            Get["/api/unicorns"] = parameters =>
+                {
+                    var imagesDirectory = HostingEnvironment.MapPath("~" + UnicornsPath);
+                    return new DirectoryInfo(imagesDirectory)
+                        .EnumerateFiles()
+                        .Select(ConvertToDto);
+                };
+        }
+
+        private dynamic ConvertToDto(FileInfo file)
+        {
+            var name = Path.GetFileNameWithoutExtension(file.Name);
+
+            return new
+                   {
+                       Key = name,
+                       ImagePath = UnicornsPath + file.Name,
+                       Name = name.Replace('_', ' '),
+                       Keywords = name.Split('_'),
+                       CreationDate = file.CreationTime
+                   };
         }
     }
 }
