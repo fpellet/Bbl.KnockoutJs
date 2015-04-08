d4707fb35e8a90caa4fbc778f2ae4be7ed6eecdb
fpellet <florent.pellet@gmail.com>
2015-04-08 23:04:55 +0200

    add search form binding

--- Bbl.KnockoutJs/Bbl.KnockoutJs/Scripts/main.js
+++ Bbl.KnockoutJs/Bbl.KnockoutJs/Scripts/main.js
@@ -1,3 +1,19 @@
-﻿$(function() {
-    
+﻿/// <reference path="jquery-2.1.3.intellisense.js" />
+/// <reference path="knockout-3.3.0.debug.js" />
+
+$(function() {
+    var SearchPageViewModel = function() {
+        var self = this;
+
+        self.value = ko.observable("");
+
+        self.search = function() {
+            alert(self.value());
+
+            return false;
+        };
+    };
+
+    var vm = new SearchPageViewModel();
+    ko.applyBindings(vm);
 });
